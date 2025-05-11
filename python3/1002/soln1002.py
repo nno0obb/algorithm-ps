0f9ebd9c880626516ec3186084a5698f9a84b283
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-12 00:08:08 +0900

    1002

---
+++ python3/1002/soln1002.py
@@ -0,0 +1,48 @@
+"""
+# 백준
+# No. 1002 
+# Python 3.11.11
+# by "nno0obb"
+"""
+
+from math import sqrt
+
+
+def logic(x1, y1, r1, x2, y2, r2):
+
+    d = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    if d == 0:
+        if r1 == r2:
+            return -1
+        elif r1 != r2:
+            return 0
+
+    if d > 0:
+        if r1 + r2 < d:
+            return 0
+        elif r1 + r2 == d:
+            return 1
+        elif abs(r1 - r2) < d < r1 + r2:
+            return 2
+        elif abs(r1 - r2) == d:
+            return 1
+        elif abs(r1 - r2) > d:
+            return 0
+
+
+def main():
+    # Input
+    T = int(input())
+    for _ in range(T):
+        x1, y1, r1, x2, y2, r2 = map(int, input().split())
+
+        # Logic
+        ans = logic(x1, y1, r1, x2, y2, r2)
+
+        # Output
+        print(ans)
+
+
+if __name__ == "__main__":
+    main()

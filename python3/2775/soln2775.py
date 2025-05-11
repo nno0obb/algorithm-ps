de498fd1a83e85a89b6ce85fa8a4e821ce315438
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-11 23:37:11 +0900

    @20250511.6 / 2775

--- python3/2775/soln2775.py
+++ python3/2775/soln2775.py
@@ -8,23 +8,28 @@
 
 def main():
     # Logic
-    # (a, b) = (a-1, 1) + (a-1, 2) + ... + (a-1, b)
-    # (a, b) = (a, b-1) + (a-1, b)
-    A = [[0 for _ in range(14 + 1)] for _ in range(14 + 1)]
-    for i in range(1, 14 + 1):
-        A[0][i] = i
-    for i in range(1, 14 + 1):
-        for j in range(1, 14 + 1):
-            A[i][j] = A[i][j - 1] + A[i - 1][j]
+    # (a, b)   = (a-1, 1) + (a-1, 2) + ... + (a-1, b-1) + (a-1, b)
+    # (a, b-1) = (a-1, 1) + (a-1, 2) + ... + (a-1, b-1)
+    # (a, b)   = (a, b-1) + (a-1, b)
+
+    max_k, max_n = 14, 14
+    apt = [[0 for _ in range(max_n + 1)] for _ in range(max_k + 1)]
+    for i in range(1, max_n + 1):
+        apt[0][i] = i
+    for i in range(1, max_k + 1):
+        for j in range(1, max_n + 1):
+            apt[i][j] = apt[i][j - 1] + apt[i - 1][j]
 
     # Input
     T = int(input())
     for _ in range(T):
-        k = int(input())
-        n = int(input())
+        k, n = int(input()), int(input())
+
+        # Logic
+        ans = apt[k][n]
 
         # Output
-        print(A[k][n])
+        print(ans)
 
 
 if __name__ == "__main__":

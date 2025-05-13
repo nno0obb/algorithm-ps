4d5b5a07e5ee5a08440d92a7ae9a1960121c8f43
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-13 22:47:01 +0900

    1003

--- boj/1003/soln1003.py
+++ boj/1003/soln1003.py
@@ -8,15 +8,22 @@
 
 def main():
     # Logic
+    max_N = 40
+    f7i = [[0, 0] for _ in range(max_N + 1)]
+    f7i[0] = [1, 0]
+    f7i[1] = [0, 1]
+
+    for i in range(2, max_N + 1):
+        f7i[i][0] = f7i[i - 1][0] + f7i[i - 2][0]
+        f7i[i][1] = f7i[i - 1][1] + f7i[i - 2][1]
 
     # Input
     T = int(input())
+    for _ in range(T):
+        N = int(input())
 
-    # Logic
-    ...
-
-    # Output
-    ...
+        # Output
+        print(f7i[N][0], f7i[N][1])
 
 
 if __name__ == "__main__":

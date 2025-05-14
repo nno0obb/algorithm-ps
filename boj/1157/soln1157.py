7837da1d42cb3135eb967b3d5cd7511b157a38bd
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-14 18:09:50 +0900

    boj/1157

---
+++ boj/1157/soln1157.py
@@ -0,0 +1,29 @@
+"""
+# 백준
+# No. 1157 
+# Python 3.11.11
+# by "nno0obb"
+"""
+
+from collections import Counter
+
+
+def main():
+    # Input
+    word = input()
+
+    # Logic
+    counter = Counter(word.upper())
+
+    ans = counter.most_common(1)[0][0]
+
+    if len(counter) > 1:
+        _1st, _2nd = counter.most_common(2)
+        if _1st[1] == _2nd[1]:
+            ans = "?"
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

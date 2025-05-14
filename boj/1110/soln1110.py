e4cec179d4c3b5bebe6c97614780130c75972785
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-14 18:00:08 +0900

    boj/1110

---
+++ boj/1110/soln1110.py
@@ -0,0 +1,30 @@
+"""
+# 백준
+# No. 1110 
+# Python 3.11.11
+# by "nno0obb"
+"""
+
+
+def cycle(n):
+    a, b = divmod(n, 10)
+    c = (a + b) % 10
+    return b * 10 + c
+
+
+def main():
+    # Input
+    N = int(input())
+
+    # Logic
+    cnt, now = 1, cycle(N)
+    while N != now:
+        now = cycle(now)
+        cnt += 1
+
+    # Output
+    print(cnt)
+
+
+if __name__ == "__main__":
+    main()

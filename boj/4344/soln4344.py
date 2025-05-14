a35bed21a773402ee6baa618895cb42665fdfd3d
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-14 23:53:00 +0900

    boj/4344

---
+++ boj/4344/soln4344.py
@@ -0,0 +1,25 @@
+"""
+# 백준
+# No. 4344 
+# Python 3.11.11
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    C = int(input())
+    for _ in range(C):
+        N, *scores = map(int, input().split())
+
+        # Logic
+        avg = sum(scores) / N
+        above_avg = [score for score in scores if score > avg]
+        rate = len(above_avg) / N * 100
+
+        # Output
+        print(f"{rate}%")
+
+
+if __name__ == "__main__":
+    main()

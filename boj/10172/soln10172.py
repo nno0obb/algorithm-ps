407243f6a53cc5bdc3b40ca8a93f84c6d65d002c
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-17 00:51:47 +0900

    boj/10172

---
+++ boj/10172/soln10172.py
@@ -0,0 +1,28 @@
+"""
+# 백준
+# No. 10172 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+
+    # Logic
+    dog = r'''
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+'''.strip(
+        "\n"
+    )
+
+    # Output
+    print(dog)
+
+
+if __name__ == "__main__":
+    main()

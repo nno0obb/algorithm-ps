0e1566e2af72921639e1c2cfc11fc1f85f540be0
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-17 00:51:55 +0900

    boj/25083

---
+++ boj/25083/soln25083.py
@@ -0,0 +1,29 @@
+"""
+# 백준
+# No. 25083 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+
+    # Logic
+    sprout = r"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+""".strip(
+        "\n"
+    )
+
+    # Output
+    print(sprout)
+
+
+if __name__ == "__main__":
+    main()

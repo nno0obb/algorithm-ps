68281f07b03f7e249c5e257938d45fea2518a419
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-17 21:56:23 +0900

    boj/1008

---
+++ boj/1008/soln1008.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 1008 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    A, B = map(int, input().split())
+
+    # Logic
+    ans = A / B
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

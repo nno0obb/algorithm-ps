83ab6fc9e87a8a8a8e105b6701548551f9b42cf0
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-18 00:43:18 +0900

    boj/2420

---
+++ boj/2420/soln2420.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 2420 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N, M = map(int, input().split())
+
+    # Logic
+    ans = abs(N - M)
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

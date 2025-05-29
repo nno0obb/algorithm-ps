79426267ac6720ec3e5d0d677ed4ac363cb0aa02
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-29 16:59:43 +0900

    boj/10951

---
+++ boj/10951/soln10951.py
@@ -0,0 +1,25 @@
+"""
+# 백준
+# No. 10951 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    while True:
+        try:
+            A, B = map(int, input().split())
+
+            # Logic
+
+            # Output
+            print(A + B)
+
+        except EOFError:
+            break
+
+
+if __name__ == "__main__":
+    main()

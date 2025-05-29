facadaf2b4ef2868436cc6b8710e7930a62dd42f
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-29 17:01:10 +0900

    boj/11654

---
+++ boj/11654/soln11654.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 11654 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    c = input()
+
+    # Logic
+    ans = ord(c)
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

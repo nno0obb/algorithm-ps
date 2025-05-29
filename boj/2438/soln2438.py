038f32c1d60b58d497c3cb678fd47747778bc2a9
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-29 17:01:11 +0900

    boj/2438

---
+++ boj/2438/soln2438.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 2438 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+
+    # Logic
+
+    # Output
+    for i in range(1, N + 1):
+        print("*" * i)
+
+
+if __name__ == "__main__":
+    main()

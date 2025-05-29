3e8730decf4f81f982daeb84a952bd39f892b653
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-05-29 17:01:11 +0900

    boj/11718

---
+++ boj/11718/soln11718.py
@@ -0,0 +1,17 @@
+"""
+# 백준
+# No. 11718 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+import sys
+
+
+def main():
+    # Input, Logic, Output
+    print(sys.stdin.read())
+
+
+if __name__ == "__main__":
+    main()

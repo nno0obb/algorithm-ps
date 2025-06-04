deabaf51fe7f966a4a71778b8f184d0cd7979840
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-06-04 23:53:47 +0900

    boj/17263

---
+++ boj/17263/soln17263.py
@@ -0,0 +1,22 @@
+"""
+# 백준
+# No. 17263 
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    # Logic
+    ans = max(A)
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

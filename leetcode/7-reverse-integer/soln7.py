fb811d1beefa1fd767545fa3a9767c23ee2cd9ad
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-07-13 16:53:17 +0900

    leetcode/7-reverse-integer

---
+++ leetcode/7-reverse-integer/soln7.py
@@ -0,0 +1,19 @@
+"""
+# 리트코드
+# No. 7 / reverse-integer
+# Python 3.x.y
+# by "nno0obb"
+"""
+
+
+class Solution:
+    ...
+
+
+def test_solution(subtests):
+    with subtests.test("Example 1"):
+        ...
+    with subtests.test("Example 2"):
+        ...
+    with subtests.test("Example 3"):
+        ...

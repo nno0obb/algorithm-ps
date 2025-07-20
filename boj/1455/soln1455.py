5140679ffb62512b6366507568a4585c108435ee
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-07-20 21:57:30 +0900

    boj/1455

---
+++ boj/1455/soln1455.py
@@ -0,0 +1,44 @@
+"""
+# 백준
+# No. 1455
+# Python 3.11.9 (PyPy3..?)
+# by "nno0obb"
+"""
+
+import sys
+
+sys.setrecursionlimit(1_000_000)
+
+
+# Global Variables
+max_N, max_M = 100, 100
+B = [[0] * max_M for _ in range(max_N)]
+
+
+def flip(n, m):
+    for i in range(n + 1):
+        for j in range(m + 1):
+            B[i][j] = 1 - B[i][j]
+
+
+def main():
+    # Input
+    N, M = map(int, input().split())
+    for i in range(N):
+        B[i] = list(map(int, list(input())))
+
+    # Logic
+    ans, idx = 0, N * M - 1
+    while idx >= 0:
+        n, m = divmod(idx, M)
+        if B[n][m] == 1:
+            ans += 1
+            flip(n, m)
+        idx -= 1
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

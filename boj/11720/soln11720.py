d0ecd2a44556ed269338a491b0923873964fcac6
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-07-24 22:26:49 +0900

    boj/11720

---
+++ boj/11720/soln11720.py
@@ -0,0 +1,22 @@
+"""
+# 백준
+# No. 11720
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+    nums = list(map(int, list(input())))
+
+    # Logic
+    ans = sum(nums)
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

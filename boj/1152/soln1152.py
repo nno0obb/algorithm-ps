aaf9c984d068aec4e26db0ca79b88fa1a230c543
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-07-26 01:09:30 +0900

    boj/1152

---
+++ boj/1152/soln1152.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 1152
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    string = input()
+
+    # Logic
+    words = string.split()
+
+    # Output
+    print(len(words))
+
+
+if __name__ == "__main__":
+    main()

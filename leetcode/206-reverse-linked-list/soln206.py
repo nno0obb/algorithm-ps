49224ccfa56357d77f8ae2f488f24d693531ae4d
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2025-08-04 23:47:03 +0900

    leetcode/206-reverse-linked-list

---
+++ leetcode/206-reverse-linked-list/soln206.py
@@ -0,0 +1,64 @@
+"""
+# 리트코드
+# No. 206 / reverse-linked-list
+# Python 3.x.y
+# by "nno0obb"
+"""
+
+from typing import List, Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, _next=None):
+        self.val = val
+        self.next = _next
+
+    def __eq__(self, other) -> bool:
+        node1, node2 = self, other
+        while node1 and node2:
+            if node1.val != node2.val:
+                return False
+            node1, node2 = node1.next, node2.next
+        return node1 is None and node2 is None
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        return prev
+
+
+def create_linked_list(values: List[int]) -> ListNode:
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+    for value in values[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+
+def test_solution(subtests):
+    with subtests.test("Example 1"):
+        head = [1, 2, 3, 4, 5]
+        head = create_linked_list(head)
+        output = [5, 4, 3, 2, 1]
+        output = create_linked_list(output)
+        assert Solution().reverseList(head) == output
+    with subtests.test("Example 2"):
+        head = [1, 2]
+        head = create_linked_list(head)
+        output = [2, 1]
+        output = create_linked_list(output)
+        assert Solution().reverseList(head) == output
+    with subtests.test("Example 3"):
+        head = []
+        head = create_linked_list(head)
+        output = []
+        output = create_linked_list(output)
+        assert Solution().reverseList(head) == output

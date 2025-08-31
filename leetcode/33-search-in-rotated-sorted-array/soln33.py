a97b42b84445e084c6ea0382cfdc3ac24350ed36
nno0obb <nno0obb@kakao.com>
2025-08-31 15:31:53 +0900

    leetcode/33-search-in-rotated-sorted-array

---
+++ leetcode/33-search-in-rotated-sorted-array/soln33.py
@@ -0,0 +1,41 @@
+"""
+# 리트코드
+# No. 33 / search-in-rotated-sorted-array
+# Python 3.x.y
+# by "nno0obb"
+"""
+
+import bisect
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        k = 0
+        for i in range(1, len(nums)):
+            if nums[i - 1] > nums[i]:
+                k = i
+                break
+
+        idx = bisect.bisect_left(nums[:k], target)
+        if idx < len(nums[:k]) and nums[idx] == target:
+            return idx
+        idx = bisect.bisect_left(nums[k:], target) + k
+        if idx < len(nums) and nums[idx] == target:
+            return idx
+        return -1
+
+
+def test_solution(subtests):
+    with subtests.test("Example 1"):
+        nums, target = [4, 5, 6, 7, 0, 1, 2], 0
+        output = 4
+        assert Solution().search(nums, target) == output
+    with subtests.test("Example 2"):
+        nums, target = [4, 5, 6, 7, 0, 1, 2], 3
+        output = -1
+        assert Solution().search(nums, target) == output
+    with subtests.test("Example 3"):
+        nums, target = [1], 0
+        output = -1
+        assert Solution().search(nums, target) == output

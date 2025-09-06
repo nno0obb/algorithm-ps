854078b4c7263973a5d21ad7c04bde511a05d421
nno0obb <nno0obb@kakao.com>
2025-09-06 09:41:45 +0900

    boj/27323

---
+++ boj/27323/soln27323.py
@@ -0,0 +1,22 @@
+"""
+# 백준
+# No. 27323
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    A = int(input())
+    B = int(input())
+
+    # Logic
+    ans = A * B
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

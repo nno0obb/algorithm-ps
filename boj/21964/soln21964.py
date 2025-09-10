645050fb89f1a75019ce4d6ef8f764f81706901d
nno0obb <nno0obb@kakao.com>
2025-09-10 19:34:21 +0900

    boj/21964

---
+++ boj/21964/soln21964.py
@@ -0,0 +1,22 @@
+"""
+# 백준
+# No. 21964
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+    S = input()
+
+    # Logic
+    ans = S[-5:]
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

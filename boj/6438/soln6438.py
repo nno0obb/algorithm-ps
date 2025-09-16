2041f28200c0fa613f337f9223083bdbe62c2a52
nno0obb <nno0obb@kakao.com>
2025-09-16 22:59:54 +0900

    boj/6438

---
+++ boj/6438/soln6438.py
@@ -0,0 +1,20 @@
+"""
+# 백준
+# No. 6438
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    T = int(input())
+    for _ in range(T):
+        S = input()
+
+        # Logic, Output
+        print(S[::-1])
+
+
+if __name__ == "__main__":
+    main()

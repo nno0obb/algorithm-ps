1e3dbce775216b991d800bd6821bea1e0f8a264b
nno0obb <nno0obb@kakao.com>
2025-09-16 22:57:28 +0900

    boj/32297

---
+++ boj/32297/soln32297.py
@@ -0,0 +1,22 @@
+"""
+# 백준
+# No. 32297
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+    S = input()
+
+    # Logic
+    ans = "YES" if "gori" in S else "NO"
+
+    # Output
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()

cfbd145198ee8ffe89424fc2ff9100e50f95812d
nno0obb <nno0obb@kakao.com>
2025-09-27 23:23:05 +0900

    boj/24263

---
+++ boj/24263/soln24263.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 24263
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+
+    # Logic
+    ans = [N, 1]
+
+    # Output
+    print(*ans, sep="\n")
+
+
+if __name__ == "__main__":
+    main()

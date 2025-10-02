703b5e88ff604ad92b75b24ac1e189fecd48d564
nno0obb <nno0obb@kakao.com>
2025-10-02 21:32:10 +0900

    boj/21553

---
+++ boj/21553/soln21553.py
@@ -0,0 +1,19 @@
+"""
+# 백준
+# No. 21553
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    A = int(input())
+    P = int(input())
+
+    # Logic, Output
+    print(P)
+
+
+if __name__ == "__main__":
+    main()

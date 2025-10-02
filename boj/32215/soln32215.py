68c0c3ccbedb944ac55a0daceee2afb3a095ea6e
nno0obb <nno0obb@kakao.com>
2025-10-02 21:46:15 +0900

    boj/32215

---
+++ boj/32215/soln32215.py
@@ -0,0 +1,18 @@
+"""
+# 백준
+# No. 32215
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N, M, K = map(int, input().split())
+
+    # Logic, Output
+    print(M * (K + 1))
+
+
+if __name__ == "__main__":
+    main()

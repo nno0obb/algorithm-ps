1fe77f3b190e0c057fae76034f6b4015eab36ff2
nno0obb <nno0obb@kakao.com>
2025-10-19 00:07:38 +0900

    boj/2121

--- boj/2121/soln2121.go
+++ boj/2121/soln2121.go
@@ -16,15 +16,34 @@ import (
 var reader = bufio.NewReader(os.Stdin)
 var writer = bufio.NewWriter(os.Stdout)
 
+type Point struct {
+	x int
+	y int
+}
+
 func main() {
 	defer writer.Flush()
 
 	// Input
-    ...
+	var N int
+	fmt.Fscanln(reader, &N)
+	var A, B int
+	fmt.Fscanln(reader, &A, &B)
+	C := make(map[Point]bool)
+	for range N {
+		var x, y int
+		fmt.Fscanln(reader, &x, &y)
+		C[Point{x, y}] = true
+	}
 
-    // Logic
-    ...
+	// Logic
+	var ans int
+	for c := range C {
+		if C[Point{c.x, c.y + B}] && C[Point{c.x + A, c.y}] && C[Point{c.x + A, c.y + B}] {
+			ans++
+		}
+	}
 
-    // Output
-    ...
+	// Output
+	fmt.Fprintln(writer, ans)
 }

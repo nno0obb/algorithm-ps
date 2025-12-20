5779b0117baac771f4a04b841a9dcdb95b7290b5
nno0obb <nno0obb@kakao.com>
2025-12-20 22:28:10 +0900

    boj/13410

---
+++ boj/13410/soln13410.py
@@ -0,0 +1,21 @@
+"""
+# 백준
+# No. 13410
+# Python 3.11.9
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    ...
+
+    # Logic
+    ...
+
+    # Output
+    ...
+
+
+if __name__ == "__main__":
+    main()

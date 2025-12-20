9deab3403cc379060918181a08f6c2decc9b96ff
nno0obb <nno0obb@kakao.com>
2025-12-20 22:31:45 +0900

    boj/13410

--- boj/13410/soln13410.py
+++ boj/13410/soln13410.py
@@ -8,13 +8,13 @@
 
 def main():
     # Input
-    ...
+    N, K = map(int, input().split())
 
     # Logic
-    ...
+    ans = max([int(str(N * k)[::-1]) for k in range(1, K + 1)])
 
     # Output
-    ...
+    print(ans)
 
 
 if __name__ == "__main__":
